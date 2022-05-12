105d36ee0fe82ebc30914d39b59f0523868a4e86
GermanoGiudici <germano.giudici@gmail.com>
2022-05-12 09:35:26 +0200

    template for vue router

--- prepareDockerImage.sh
+++ prepareDockerImage.sh
@@ -4,7 +4,7 @@ echo ""
 echo "Building project and Docker image "
 #todo export as global variable
 #todo manage version
-export MY_IMAGE=CHANGE_IT/node-kc-microservice:0.0.1-SNAPSHOT
+export MY_IMAGE=CHANGE-IT/node-kc-microservice:0.0.1-SNAPSHOT
 
 docker build -t ${MY_IMAGE}  .
 echo "Built $MY_IMAGE"
